7a9e0a3eef615aac2ebb520f638cf601ca10e628
Xasbus <davidbilliot0828.gmail.com>
2024-12-28 10:58:14 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+const root = createRoot(window.bodyTag);
+
+root.render(
+  <main>
+    <h1>Test</h1>
+    <p>Paragraph Testing</p>
+  </main>
+);
