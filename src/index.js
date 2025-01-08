1ea841f718996cd99223cc258452d202e6bdf588
Xasbus <davidbilliot0828.gmail.com>
2025-01-07 21:59:49 -0600

    Beginning base project

--- src/index.js
+++ src/index.js
@@ -9,17 +9,51 @@ root.render(
       <nav></nav>
     </header>
     <main>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <h1>
-              Redmi Turbo 3<br></br>Harry Potter Limited Edition
-            </h1>
-          </div>
-          <div class="col">CANT RENDER PICTURE</div>
-        </div>
+      <div class="topPic">
+        <h1 class="">
+          Redmi Turbo 3<br></br>Harry Potter Limited Edition
+        </h1>
+        <br></br>
+        <h3 class="">
+          This Harry Potter Xiaomi Redmi Turbo 3<br /> actually looks worthy of
+          wizarding world
+        </h3>
       </div>
+      <section class="container">
+        <h2>
+          Who needs a wand when you've <br />
+          got a Harry Potter-themed phone
+        </h2>
+
+        <p>
+          Xiaomi has unveiled a magical-looking Harry Potter edition of the
+          Xiaomi Redmi Turbo 3 that could enchant fans of the books and films.
+        </p>
+        <p>
+          The Harry Potter edition of the latest Redmi Turbo 3 was revealed in
+          China, although one version is going on sale globally. It features a
+          host of Harry Potter-themed embellishments, including an ornate Diagon
+          Alley-style presentation box with a Hogwarts school badge pin, a
+          themed case, a custom charger, and themed animations and wallpapers.
+        </p>
+        <p>
+          The Redmi Turbo 3's design features a vibrant blue and gold back panel
+          with a prominent Hogwarts emblem at the forefront, accompanied by the
+          inscription "Harry Potter" below it. The camera flash also has a
+          lightning bolt with a wand and the word "Lumos" next to it. The blue
+          section features illustrations of Harry Potter lore, including the
+          dreaded Horcruxes.
+        </p>
+        <h6>About the Phone</h6>
+        <ul>
+          <li>6.67-inch 120Hz OLED display.</li>
+          <li>Qualcomm Snapdragon 8s Gen 3 processor.</li>
+          <li>16GB LPDDR5X RAM, 512GB UFS 4.0 storage.</li>
+          <li>50MP+8MP rear dual-cameras, a 16MP selfie camera.</li>
+          <li>5000mAh battery, 90W Fast Flash Charge.</li>
+        </ul>
+      </section>
     </main>
-    <footer>Website is created by David Billiot</footer>
+    <footer class="container">Website is created by David Billiot</footer>
   </body>
 );
